a4708d41dcfdeb13268c8be61e8efa57f1b6dad6
Kanjirito <kanjirito@protonmail.com>
2021-01-15 11:43:36 +0100

    Moved from using setup.py to setup.cfg and pyproject.toml
    Moved setuptools options from setup.py to setup.cfg for easier reading.
    Added pyproject.toml to follow PEP518, setup.py is now only present to allow
    editable installs.
    Will revert back if something breaks.

--- setup.py
+++ setup.py
@@ -1,36 +1,5 @@
+#!/usr/bin/env python
 import setuptools
-from simple_manga_downloader import __version__
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-required = [
-    "requests~=2.23",
-    "beautifulsoup4~=4.9"
-]
-
-setuptools.setup(
-    name="simple-manga-downloader",
-    version=__version__,
-    author="Kanjirito",
-    author_email="kanjirito@protonmail.com",
-    license="GPLv3",
-    url="https://github.com/Kanjirito/simple-manga-downloader",
-    description="Simple manga console downloader",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    install_requires=required,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Environment :: Console"
-    ],
-    python_requires=">=3.6",
-    entry_points={
-        "console_scripts": [
-            "SMD=simple_manga_downloader.SMD:main"
-        ]
-    }
-)
+if __name__ == "__main__":
+    setuptools.setup()
